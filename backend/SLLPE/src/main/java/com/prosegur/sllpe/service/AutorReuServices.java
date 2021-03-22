b8cb7413430eec0592ec9e481805d7397d31098f
nzambrano <ext-noel-zambrano@gmail.com>
2021-03-22 19:42:49 -0300

    servicio autor reu

---
+++ backend/SLLPE/src/main/java/com/prosegur/sllpe/service/AutorReuServices.java
@@ -0,0 +1,33 @@
+package com.prosegur.sllpe.service;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.prosegur.sllpe.domain.M4sllAutorReu;
+import com.prosegur.sllpe.repository.M4sllAutorReuRepository;
+
+
+@Service
+@Transactional
+
+public class AutorReuServices {
+
+	private final M4sllAutorReuRepository M4sllAutorReuRepository;
+	
+	public AutorReuServices(M4sllAutorReuRepository m4ccoLlPagosRepository) {
+		this.M4sllAutorReuRepository = m4ccoLlPagosRepository;
+	}
+	
+	public boolean casteoError(M4sllAutorReu autorReu) {
+		return autorReu != null ? true: false;
+ 	}
+	
+	
+	public  Long UltimaSecuencia(M4sllAutorReu autorReu) {
+		return  M4sllAutorReuRepository.obtenerUltimaSecuencia(autorReu.getId().getLitIdLitigio(), autorReu.getId().getIdOrganization());
+	}
+	
+	
+}
---
+++ backend/SLLPE/src/main/java/com/prosegur/sllpe/web/rest/M4sllAutorReuResource.java
@@ -0,0 +1,139 @@
+package com.prosegur.sllpe.web.rest;
+
+import com.prosegur.sllpe.domain.M4sllAutorReu;
+import com.prosegur.sllpe.domain.M4sllAutorReuId;
+import com.prosegur.sllpe.repository.M4sllAutorReuRepository;
+import com.prosegur.sllpe.service.AutorReuServices;
+import com.prosegur.sllpe.web.rest.errors.BadRequestAlertException;
+
+import io.github.jhipster.web.util.HeaderUtil;
+// import io.github.jhipster.web.util.PaginationUtil;
+import io.github.jhipster.web.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.Pageable;
+// import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@Transactional
+
+public class M4sllAutorReuResource {
+
+    private final Logger log = LoggerFactory.getLogger(M4sllAutorReuResource.class);
+    private static final String ENTITY_NAME = "sllpeM4sllAutorReu";
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+    private M4sllAutorReuRepository m4sllAutorReuRepository;
+
+    public M4sllAutorReuResource(M4sllAutorReuRepository m4sllAutorReuRepository) {
+        this.m4sllAutorReuRepository = m4sllAutorReuRepository;
+    }
+
+    @PostMapping("/m4sll_autor_reu")
+    public ResponseEntity<M4sllAutorReu> createM4sllAutorReu(@RequestBody M4sllAutorReu m4sll_autor_reu)
+            throws URISyntaxException {
+        log.debug("REST request to create m4sll_autor_reu : {}", m4sll_autor_reu);
+        M4sllAutorReuId id = new M4sllAutorReuId();
+        AutorReuServices autorReuServices = new AutorReuServices(m4sllAutorReuRepository);
+        Long id_aurSecuencia = autorReuServices.UltimaSecuencia(m4sll_autor_reu);
+        
+        id.setAurSecuencia(id_aurSecuencia);
+        id.setLitIdLitigio(m4sll_autor_reu.getId().getLitIdLitigio());
+        id.setIdOrganization(m4sll_autor_reu.getId().getIdOrganization());
+        
+        m4sll_autor_reu.setId(id);
+        M4sllAutorReu result = m4sllAutorReuRepository.save(m4sll_autor_reu);
+        return ResponseEntity
+                .created(new URI("/api/m4sll_autor_reu/" + result.getId())).headers(HeaderUtil
+                        .createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+                .body(result);
+    }
+
+    @PutMapping("/m4sll_autor_reu")
+    public ResponseEntity<M4sllAutorReu> updateM4sllAutorReu(@RequestBody M4sllAutorReu m4sll_autor_reu)
+            throws URISyntaxException {
+        log.debug("REST request to update m4sll_autor_reu : {}", m4sll_autor_reu);
+        if (m4sll_autor_reu.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        M4sllAutorReu result = m4sllAutorReuRepository.save(m4sll_autor_reu);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME,
+                m4sll_autor_reu.getId().toString())).body(result);
+    }
+
+    @GetMapping("/m4sll_autor_reu")
+    public ResponseEntity<List<M4sllAutorReu>> getAllM4sllAutorReu() {
+        log.debug("REST request to get ALL M4sllAutorReu : {}");
+
+        List<M4sllAutorReu> M4sllAutorReuAll = m4sllAutorReuRepository.findAll();
+        return ResponseEntity.ok().body(M4sllAutorReuAll);
+    }
+    
+    @GetMapping("/m4sll_autor_reu/{id_organization}/{lit_id_litigio}/{aur_autor_reu}")
+    public ResponseEntity<List<M4sllAutorReu>> getAutoresByLitigio(
+    		@PathVariable("id_organization") String id_organization,
+    		@PathVariable("lit_id_litigio") String lit_id_litigio,
+    		@PathVariable("aur_autor_reu") String aur_autor_reu
+            
+            ) {
+        log.debug("REST request to get ALL M4sllAutorReu : {}");
+
+        List<M4sllAutorReu> M4sllAutorReuAll = m4sllAutorReuRepository.findAutorReuByLitigio(
+        		id_organization,
+        		lit_id_litigio,
+        		aur_autor_reu
+        		
+        		);
+        return ResponseEntity.ok().body(M4sllAutorReuAll);
+    }
+
+    /*
+    @GetMapping("/m4sll_autor_reu/{id_organization}/{lit_id_litigio}/{aur_secuencia}")
+    public ResponseEntity<M4sllAutorReu> getM4sllAutorReu(
+    		@PathVariable("id_organization") String idOrganization,
+    		@PathVariable("lit_id_litigio") String litIdLitigio,
+    		@PathVariable("aur_secuencia") Integer aurSecuencia    		
+            ) {
+        log.debug("REST request to get m4sll_autor_reu : {} | {}", litIdLitigio, idOrganization, aurSecuencia );
+        M4sllAutorReuId id = new M4sllAutorReuId();
+        id.setIdOrganization(idOrganization);
+        id.setLitIdLitigio(litIdLitigio);        
+        id.setAurSecuencia(aurSecuencia);
+
+        Optional<M4sllAutorReu> m4sll_autor_reu = m4sllAutorReuRepository.findById(id);
+        return ResponseUtil.wrapOrNotFound(m4sll_autor_reu);
+    }
+    */
+
+    @DeleteMapping("/m4sll_autor_reu/{id_organization}/{lit_id_litigio}/{aur_secuencia}")
+    public ResponseEntity<Void> deleteM4sllAutorReu(
+    		@PathVariable("id_organization") String idOrganization,
+            @PathVariable("lit_id_litigio") String litIdLitigio,
+            @PathVariable("aur_secuencia") Integer  aurSecuencia
+            ) {
+        log.debug("REST request to delete m4sll_autor_reu : {} | {}", litIdLitigio, idOrganization, aurSecuencia );
+        M4sllAutorReuId id = new M4sllAutorReuId();
+        id.setIdOrganization(idOrganization);
+        id.setLitIdLitigio(litIdLitigio);
+        id.setAurSecuencia(aurSecuencia);
+        
+
+        m4sllAutorReuRepository.deleteById(id);
+        return ResponseEntity.noContent()
+                .headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString()))
+                .build();
+    }
+}
