3fe09a52b15cb829036a759c59f474981a9f663d
ARX0001181 <victor.moncada@ext.prosegur.com>
2021-03-26 15:15:10 -0300

    query

--- backend/SLLPE/src/main/java/com/prosegur/sllpe/repository/M4sllTpFasesRepository.java
+++ backend/SLLPE/src/main/java/com/prosegur/sllpe/repository/M4sllTpFasesRepository.java
@@ -13,7 +13,7 @@ import com.prosegur.sllpe.domain.M4sllTpFasesId;
 
 public interface M4sllTpFasesRepository extends JpaRepository<M4sllTpFases, M4sllTpFasesId> {
 
-	@Query(value = "select COALESCE(cast( max(tfa_id_tp_fase) as integer),0)+1 as secuencia from sll.m4sll_tp_fases where id_organization = :id_organization", nativeQuery = true)
+	@Query(value = "select COALESCE(max(cast(tfa_id_tp_fase as integer)),0)+1 as secuencia from sll.m4sll_tp_fases where id_organization = :id_organization", nativeQuery = true)
 	
 	public Long obtenerUltimaSecuencia(
             @Param("id_organization") String id_organization);
