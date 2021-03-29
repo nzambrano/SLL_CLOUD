4b1e094c7fdd35ec654917af090ac90ca1272d78
nzambrano <ext-noel-zambrano@gmail.com>
2021-03-29 16:49:12 -0300

    prueba servicio

---
+++ backend/SLLPE/src/main/java/com/prosegur/sllpe/repository/M4sllMtoCodAltRepository.java
@@ -0,0 +1,9 @@
+package com.prosegur.sllpe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prosegur.sllpe.domain.M4sllMtoCodAlt;
+import com.prosegur.sllpe.domain.M4sllMtoCodAltId;
+
+public interface M4sllMtoCodAltRepository extends JpaRepository<M4sllMtoCodAlt, M4sllMtoCodAltId> {
+}
