956576097d5aa8a1162bb00e5bab3242dd2771e6
Mariano <mcoutada@gmail.com>
2021-04-15 13:07:48 -0300

    pagos rest y entidad actualizaciones

--- backend/SLLPE/src/main/java/com/prosegur/sllpe/web/rest/M4sllPagosResource.java
+++ backend/SLLPE/src/main/java/com/prosegur/sllpe/web/rest/M4sllPagosResource.java
@@ -21,12 +21,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @Transactional
 public class M4sllPagosResource {
-
     private final Logger log = LoggerFactory.getLogger(M4sllPagosResource.class);
     private static final String ENTITY_NAME = "sllpeM4sllPagos";
 
     @Value("${jhipster.clientApp.name}")
     private String applicationName;
+
+    @Value("${idOrganization}")
+    private String idOrganization;
 
     private M4sllPagosRepository m4sllPagosRepository;
 
@@ -35,36 +37,25 @@ public class M4sllPagosResource {
     }
 
     @PostMapping("/m4sll_pagos")
-    public ResponseEntity<M4sllPagos> createM4sllPagos(@RequestBody M4sllPagos m4sll_pagos)
+    public ResponseEntity<List<M4sllPagos>> createM4sllPagos(@RequestBody List<M4sllPagos> listM4sllPagos)
     throws URISyntaxException {
-        log.debug("REST request to create m4sll_pagos : {}", m4sll_pagos);
-        M4sllPagosId id = new M4sllPagosId();
-        M4sllPagosServices m4sllPagosServices = new M4sllPagosServices(m4sllPagosRepository);
-        Long id_pag_secuencia = m4sllPagosServices.UltimaSecuencia(m4sll_pagos);
-
-        id.setPagSecuencia(id_pag_secuencia);
-        id.setIdOrganization(m4sll_pagos.getId().getIdOrganization());
-        id.setLitIdLitigio(m4sll_pagos.getId().getLitIdLitigio());
-
-        m4sll_pagos.setId(id);
-        M4sllPagos result = m4sllPagosRepository.save(m4sll_pagos);
+        log.debug("REST request to create m4sll_pagos : {}", listM4sllPagos);
+        List<M4sllPagos> result = m4sllPagosRepository.saveAll(listM4sllPagos);
         return ResponseEntity
-               .created(new URI("/api/m4sll_pagos/" + result.getId()))
-               .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+               .created(new URI("/api/m4sll_pedidos/")).headers(HeaderUtil
+                       .createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.toString()))
                .body(result);
     }
 
+
     @PutMapping("/m4sll_pagos")
-    public ResponseEntity<M4sllPagos> updateM4sllPagos(@RequestBody M4sllPagos m4sll_pagos)
+    public ResponseEntity<List<M4sllPagos>> updateM4sllPagos(@RequestBody List<M4sllPagos> listM4sllPagos)
     throws URISyntaxException {
-        log.debug("REST request to update m4sll_pagos : {}", m4sll_pagos);
-        if (m4sll_pagos.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        M4sllPagos result = m4sllPagosRepository.save(m4sll_pagos);
+        log.debug("REST request to update m4sll_pagos : {}", listM4sllPagos);
+        List<M4sllPagos> result = m4sllPagosRepository.saveAll(listM4sllPagos);
         return ResponseEntity
-               .ok()
-               .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, m4sll_pagos.getId().toString()))
+               .created(new URI("/api/m4sll_pedidos/")).headers(HeaderUtil
+                       .createEntityUpdateAlert(applicationName, false, ENTITY_NAME, result.toString()))
                .body(result);
     }
 
@@ -76,47 +67,23 @@ public class M4sllPagosResource {
         return ResponseEntity.ok().body(M4sllPagosAll);
     }
 
-    @GetMapping("/m4sll_pagos/{id_organization}/{lit_id_litigio}")
-    public ResponseEntity<List<M4sllPagos>> getM4sllPagos(@PathVariable("id_organization") String id_organization, @PathVariable("lit_id_litigio") String lit_id_litigio) {
-        log.debug("REST request to get M4sllPagos : {}", id_organization + "|" + lit_id_litigio);
+    @GetMapping("/m4sll_pagos/{lit_id_litigio}")
+    public ResponseEntity<List<M4sllPagos>> getM4sllPagos(@PathVariable("lit_id_litigio") String litIdLitigio) {
+        log.debug("REST request to get M4sllPagos : {}", idOrganization + "|" + litIdLitigio);
 
-        List<M4sllPagos> M4sllPagosByInput = m4sllPagosRepository.findByIdOrganizationLitIdLitigio(id_organization, lit_id_litigio);
+        List<M4sllPagos> M4sllPagosByInput = m4sllPagosRepository.findByIdOrganizationLitIdLitigio(idOrganization, litIdLitigio);
         return ResponseEntity.ok().body(M4sllPagosByInput);
     }
 
 
-    /*
-      @GetMapping("/m4sll_pagos/{id_organization}/{lit_id_litigio}/{pag_secuencia}")
-      public ResponseEntity<M4sllPagos> getM4sllPagos(@PathVariable("id_organization") String id_organization, @PathVariable("lit_id_litigio") String lit_id_litigio, @PathVariable("pag_secuencia") Long pag_secuencia) {
-          log.debug("REST request to get M4sllPagos : {}", id_organization + "|" + lit_id_litigio + "|" + pag_secuencia);
-          M4sllPagosId id = new M4sllPagosId();
-          id.setIdOrganization(id_organization); id.setLitIdLitigio(lit_id_litigio);
-          id.setPagSecuencia(pag_secuencia);
 
-          Optional<M4sllPagos> m4sll_pagos = m4sllPagosRepository.findById(id);
-          return ResponseUtil.wrapOrNotFound(m4sll_pagos);
-      }
-
-      @DeleteMapping("/m4sll_pagos/{id_organization}/{lit_id_litigio}")
-      public ResponseEntity<Void> deleteM4sllPagos(@PathVariable("id_organization") String id_organization, @PathVariable("lit_id_litigio") String lit_id_litigio) {
-        log.debug("REST request to delete m4sll_pagos : {}", id_organization + "|" + lit_id_litigio);
-        List<M4sllPagos> M4sllPagosByInput = m4sllPagosRepository.findByIdOrganizationLitIdLitigio(id_organization, lit_id_litigio);
-
-        m4sllPagosRepository.deleteAll(M4sllPagosByInput);
-        return ResponseEntity
-          .noContent()
-          .headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, M4sllPagosByInput.toString()))
-          .build();
-    }
-    */
-
-    @DeleteMapping("/m4sll_pagos/{id_organization}/{lit_id_litigio}/{pag_secuencia}")
-    public ResponseEntity<Void> deleteM4sllPagos(@PathVariable("id_organization") String id_organization, @PathVariable("lit_id_litigio") String lit_id_litigio, @PathVariable("pag_secuencia") Long pag_secuencia) {
-        log.debug("REST request to delete m4sll_pagos : {}", id_organization + "|" + lit_id_litigio + "|" + pag_secuencia);
+    @DeleteMapping("/m4sll_pagos/{lit_id_litigio}/{pag_secuencia}")
+    public ResponseEntity<Void> deleteM4sllPagos(@PathVariable("lit_id_litigio") String litIdLitigio, @PathVariable("pag_secuencia") Long pagSecuencia) {
+        log.debug("REST request to delete m4sll_pagos : {}", idOrganization + "|" + litIdLitigio + "|" + pagSecuencia);
         M4sllPagosId id = new M4sllPagosId();
-        id.setIdOrganization(id_organization);
-        id.setLitIdLitigio(lit_id_litigio);
-        id.setPagSecuencia(pag_secuencia);
+        id.setIdOrganization(idOrganization);
+        id.setLitIdLitigio(litIdLitigio);
+        id.setPagSecuencia(pagSecuencia);
 
         m4sllPagosRepository.deleteById(id);
         return ResponseEntity
@@ -125,3 +92,66 @@ public class M4sllPagosResource {
                .build();
     }
 }
+/*
+// PostMapping para un solo registro
+  @PostMapping("/m4sll_pagos")
+  public ResponseEntity<M4sllPagos> createM4sllPagos(@RequestBody M4sllPagos m4sllPagos)
+    throws URISyntaxException {
+    log.debug("REST request to create m4sll_pagos : {}", m4sllPagos);
+    M4sllPagosId id = new M4sllPagosId();
+    M4sllPagosServices m4sllPagosServices = new M4sllPagosServices(m4sllPagosRepository);
+    Long id_pagSecuencia = m4sllPagosServices.UltimaSecuencia(m4sllPagos);
+
+    id.setPagSecuencia(id_pagSecuencia);
+    id.setIdOrganization(m4sllPagos.getId().getIdOrganization()); id.setLitIdLitigio(m4sllPagos.getId().getLitIdLitigio());
+
+    m4sllPagos.setId(id);
+    M4sllPagos result = m4sllPagosRepository.save(m4sllPagos);
+    return ResponseEntity
+      .created(new URI("/api/m4sll_pagos/" + result.getId()))
+      .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+      .body(result);
+  }
+
+// GetMapping para un solo registro
+  @GetMapping("/m4sll_pagos/{lit_id_litigio}/{pag_secuencia}")
+  public ResponseEntity<M4sllPagos> getM4sllPagos(@PathVariable("lit_id_litigio") String litIdLitigio, @PathVariable("pag_secuencia") Long pagSecuencia) {
+      log.debug("REST request to get M4sllPagos : {}", idOrganization + "|" + litIdLitigio + "|" + pagSecuencia);
+      M4sllPagosId id = new M4sllPagosId();
+      id.setIdOrganization(idOrganization); id.setLitIdLitigio(litIdLitigio);
+      id.setPagSecuencia(pagSecuencia);
+
+      Optional<M4sllPagos> m4sllPagos = m4sllPagosRepository.findById(id);
+      return ResponseUtil.wrapOrNotFound(m4sllPagos);
+  }
+
+// DeleteMapping para muchos registros de una misma combinacion (se excluye la columna de secuencia)
+  @DeleteMapping("/m4sll_pagos/{lit_id_litigio}")
+  public ResponseEntity<Void> deleteM4sllPagos(@PathVariable("lit_id_litigio") String litIdLitigio) {
+    log.debug("REST request to delete m4sll_pagos : {}", idOrganization + "|" + litIdLitigio);
+    List<M4sllPagos> M4sllPagosByInput = m4sllPagosRepository.findByIdOrganizationLitIdLitigio(idOrganization, litIdLitigio);
+
+    m4sllPagosRepository.deleteAll(M4sllPagosByInput);
+    return ResponseEntity
+      .noContent()
+      .headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, M4sllPagosByInput.toString()))
+      .build();
+}
+
+// PutMapping para un registro
+@PutMapping("/m4sll_pagos")
+public ResponseEntity<M4sllPagos> updateM4sllPagos(@RequestBody M4sllPagos m4sllPagos)
+  throws URISyntaxException {
+  log.debug("REST request to update m4sll_pagos : {}", m4sllPagos);
+  if (m4sllPagos.getId() == null) {
+    throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+  }
+  M4sllPagos result = m4sllPagosRepository.save(m4sllPagos);
+  return ResponseEntity
+    .ok()
+    .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, m4sllPagos.getId().toString()))
+    .body(result);
+}
+
+
+*/
